8cd7acb39722715fb89d61c0da1eb357cd521538
Vasiliy Makarov <drmoriarty.0@gmail.com>
2018-12-17 12:18:45 +0300

    update libraries

--- app/jni/admob/Android.mk
+++ app/jni/admob/Android.mk
@@ -3,9 +3,9 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := admob
 ifeq ($(APP_STL), c++_static)
-    LOCAL_SRC_FILES := libs_c++_static/$(TARGET_ARCH_ABI)/libadmob.a
+    LOCAL_SRC_FILES := libs_c++_static/$(TARGET_ARCH_ABI)/libfirebase_admob.a
 else
-    LOCAL_SRC_FILES := libs/$(TARGET_ARCH_ABI)/libadmob.a
+    LOCAL_SRC_FILES := libs/$(TARGET_ARCH_ABI)/libfirebase_admob.a
 endif
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
 include $(PREBUILT_STATIC_LIBRARY)
